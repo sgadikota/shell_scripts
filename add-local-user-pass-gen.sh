e2b28ab9cb67b89fe9142a14f61729b97aebb2f6
swaroop <swaroop@swaroops-MBP.fios-router.home>
2021-02-12 17:56:55 -0500

    creating user account and also setting random password, printing out password, check if script run by root user, check if account created

---
+++ add-local-user-pass-gen.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# This script is to create a user when we run this script.
+
+# Make sure the script is being executed with superuser privileges. else it will return exit status 1
+if [[ ${UID} -eq 0 ]]
+then
+  echo "Root user is running the script"
+else
+  echo "Run with Sudo or root user"
+  exit 1
+fi
+
+# Get the username (login).
+read -p 'Please enter your account name: ' ACCOUNT_NAME
+
+# prints empty line
+echo
+
+# prints accnt name entered
+echo "***********  Entered account name is: "${ACCOUNT_NAME}""
+
+# Make sure provide account name.
+# type -a test
+# help test
+# check usage of -z and -n if the value of string is null or not.
+
+if [[ -z "${ACCOUNT_NAME}" ]]
+then
+  echo "U have not passed account name, please see the usage below"
+  echo "Usage: ${0} ACCOUNT_NAME [ACCOUNT_NAME]..."
+  exit 1
+fi
+
+
+# getting username as first word from account name entered
+USER_NAME="$(echo ${ACCOUNT_NAME} | head -n1 | awk '{print $1;}')"
+echo "username going to be created is: ${USER_NAME}"
+
+# except first word everything is taken as comment
+COMMENT="$(echo ${ACCOUNT_NAME} | sed "s/^[^ ]* //")"
+echo "comment to be updated is: ${COMMENT}"
+
+# creating an user, man useradd --> -c for comment ; -m for creating home directory if it doen't exist.
+useradd -c "${COMMENT}" -m ${USER_NAME}
+
+# Use the current date/time as the basis for the password.
+PASSWORD=$(date +%s)
+echo "Password: ${PASSWORD}"
+
+# Set the password for the user.
+echo ${PASSWORD} | passwd --stdin ${USER_NAME}
+
+
+# check if the user is created using the id command; id -u name
+# type -a id; man id;
+
+ID="$(id -u ${USER_NAME})"
+
+if [[ ${ID} -eq 1000 ]]
+then
+  echo "User account is not created somehow"
+  exit 1
+else
+  echo "User account is created"
+fi
+
+# Display the username, password, and the host where the user was created.
+echo username: "${USER_NAME}"
+echo password: "${PASSWORD}"
+echo hostname: $(hostname)
+exit 0
